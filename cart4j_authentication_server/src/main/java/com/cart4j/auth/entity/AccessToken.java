45a1b1b9fcf713688a8916369939fb4a98fbef20
mjjmmjj <mjjmmjj@gmail.com>
2018-11-28 23:17:32 -0600

    init authentication server

---
+++ cart4j_authentication_server/src/main/java/com/cart4j/auth/entity/AccessToken.java
@@ -0,0 +1,12 @@
+package com.cart4j.auth.entity;
+
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="c4_access_token")
+
+public class AccessToken {
+}
