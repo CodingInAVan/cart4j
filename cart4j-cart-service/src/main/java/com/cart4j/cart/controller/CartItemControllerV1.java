7fb1faa897e3a0fecd33b514742926e3896026b2
mjjmmjj <mjjmmjj@gmail.com>
2019-01-27 22:10:32 -0600

    InvalidCartException has been added to controller

--- cart4j-cart-service/src/main/java/com/cart4j/cart/controller/CartItemControllerV1.java
+++ cart4j-cart-service/src/main/java/com/cart4j/cart/controller/CartItemControllerV1.java
@@ -2,14 +2,16 @@ package com.cart4j.cart.controller;
 
 
 import com.cart4j.cart.dto.v1.CartItemDtoV1;
+import com.cart4j.cart.exception.InvalidCartException;
 import com.cart4j.cart.service.v1.CartItemServiceV1;
+import com.cart4j.common.dto.ErrorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/cart-item/v1")
@@ -23,17 +25,22 @@ public class CartItemControllerV1 {
     }
 
     @PostMapping("/cart/{cartId}")
-    CartItemDtoV1 addCartItem(Principal principal, @PathVariable Long cartId, @RequestBody CartItemDtoV1 cartItem) {
+    CartItemDtoV1 addCartItem(Principal principal, @PathVariable Long cartId, @RequestBody CartItemDtoV1 cartItem) throws InvalidCartException {
         return cartItemServiceV1.addCartItem(principal.getName(), cartId, cartItem);
     }
 
     @PutMapping("/{cartItemId}")
-    CartItemDtoV1 editCartItem(Principal principal, @PathVariable Long cartItemId, @RequestBody CartItemDtoV1 cartItem) {
+    CartItemDtoV1 editCartItem(Principal principal, @PathVariable Long cartItemId, @RequestBody CartItemDtoV1 cartItem) throws InvalidCartException {
         return cartItemServiceV1.updateCartItem(principal.getName(), cartItemId, cartItem);
     }
 
-    @DeleteMapping("/ids/{cartItemIds}")
-    void deleteCartItems(Principal principal, @PathVariable String cartItemIds) {
-        cartItemServiceV1.deleteCartItems(principal.getName(), Stream.of(cartItemIds.split(",")).map(Long::valueOf).collect(Collectors.toList()));
+    @DeleteMapping("/{cartItemId}")
+    void deleteCartItems(Principal principal, @PathVariable Long cartItemId) throws InvalidCartException {
+        cartItemServiceV1.deleteCartItem(principal.getName(), cartItemId);
+    }
+
+    @ExceptionHandler({InvalidCartException.class})
+    ResponseEntity<ErrorResponse> handleInvalidCartException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse.builder().errorCode(HttpStatus.PRECONDITION_FAILED.value()).message(e.getMessage()).build());
     }
 }
