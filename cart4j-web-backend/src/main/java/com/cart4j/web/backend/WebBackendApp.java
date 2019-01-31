e084533b89cdec659e37067f54578915a2402348
mjjmmjj <mjjmmjj@gmail.com>
2019-01-31 08:28:58 -0600

    Add web backend api

---
+++ cart4j-web-backend/src/main/java/com/cart4j/web/backend/WebBackendApp.java
@@ -0,0 +1,11 @@
+package com.cart4j.web.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBackendApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WebBackendApp.class, args);
+    }
+}
