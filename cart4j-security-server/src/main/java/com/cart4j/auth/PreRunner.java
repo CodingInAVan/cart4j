c0e4e56bb9f19c42ead7389cb2d8613df4bd16db
mjjmmjj <mjjmmjj@gmail.com>
2019-03-14 20:37:21 -0500

    Remove unused imports

--- cart4j-security-server/src/main/java/com/cart4j/auth/PreRunner.java
+++ cart4j-security-server/src/main/java/com/cart4j/auth/PreRunner.java
@@ -1,10 +1,5 @@
 package com.cart4j.auth;
 
-import com.cart4j.auth.dto.ClientDto;
-import com.cart4j.auth.dto.RoleDto;
-import com.cart4j.auth.dto.ScopeDto;
-import com.cart4j.auth.dto.UserDto;
-import com.cart4j.auth.repository.UserRepository;
 import com.cart4j.auth.service.ClientService;
 import com.cart4j.auth.service.RoleService;
 import com.cart4j.auth.service.ScopeService;
@@ -13,19 +8,12 @@ import com.cart4j.model.security.dto.v1.ClientDto;
 import com.cart4j.model.security.dto.v1.RoleDto;
 import com.cart4j.model.security.dto.v1.ScopeDto;
 import com.cart4j.model.security.dto.v1.UserDto;
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
-import java.io.InputStream;
-import java.nio.charset.Charset;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Scanner;
 
 @Component
@@ -33,14 +21,18 @@ public class PreRunner implements CommandLineRunner {
     @Value("${install}")
     private Boolean install;
 
+    private final UserService userService;
+    private final ClientService clientService;
+    private final ScopeService scopeService;
+    private final RoleService roleService;
+
     @Autowired
-    private UserService userService;
-    @Autowired
-    private ClientService clientService;
-    @Autowired
-    private ScopeService scopeService;
-    @Autowired
-    private RoleService roleService;
+    public PreRunner(UserService userService, ClientService clientService, ScopeService scopeService, RoleService roleService) {
+        this.userService = userService;
+        this.clientService = clientService;
+        this.scopeService = scopeService;
+        this.roleService = roleService;
+    }
 
     @Override
     public void run(String... args) throws Exception {
